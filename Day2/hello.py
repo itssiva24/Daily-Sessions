0299535baf0b72e9125e161e525c4470b1cd3143
Siva <sivaiitm24@gmail.com>
2020-03-27 16:48:05 +0530

    Check input values

--- Day2/hello.py
+++ Day2/hello.py
@@ -6,10 +6,12 @@ def n_th_term_of_fibanocci(n):
 
 
 n = int(input("Enter number of lines: "))
-
-for line in range(1, n+1):
-    x = int((line-1)*line/2) + 1
-    y = int((line+1)*line/2) + 1
-    for z in range(x, y):
-        print(n_th_term_of_fibanocci(z), end=" ")
-    print("\n")
+if n > 0:
+    for line in range(1, n+1):
+        x = int((line-1)*line/2) + 1
+        y = int((line+1)*line/2) + 1
+        for z in range(x, y):
+            print(n_th_term_of_fibanocci(z), end=" ")
+        print("\n")
+else:
+    print("Please enter positive values")

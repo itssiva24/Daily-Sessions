b3402956c77f3372caf4723723dba1a444dfae23
Siva <sivaiitm24@gmail.com>
2020-03-27 16:22:20 +0530

    Add hello.py

---
+++ Day2/hello.py
@@ -0,0 +1,15 @@
+def n_th_term_of_fibanocci(n):
+    if n in (1, 2):
+        return 1
+    else:
+        return n_th_term_of_fibanocci(n-1) + n_th_term_of_fibanocci(n-2)
+
+
+n = int(input("Enter number of lines: "))
+
+for line in range(1, n+1):
+    x = int((line-1)*line/2) + 1
+    y = int((line+1)*line/2) + 1
+    for z in range(x, y):
+        print(n_th_term_of_fibanocci(z), end=" ")
+    print("\n")
